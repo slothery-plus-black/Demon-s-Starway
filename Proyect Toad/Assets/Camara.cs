919b54e2b0097481d3d24c6d717767d1e0df1a0c
Jaime Calvo <jaime230593@hotmail.es>
2017-12-21 03:54:25 +0100

    versiones

--- Proyect Toad/Assets/Camara.cs
+++ Proyect Toad/Assets/Camara.cs
@@ -14,8 +14,30 @@ public class Camara : MonoBehaviour {
     private float yaw = 0.0f;
     private float pitch = 0.0f;
 
+    bool pulsado = false;
+
+    Vector3 posPulsado = Vector3.zero;
+
     void Update () {
+        
+
         if (Input.touchCount > 0){
+            HandleTouch(Input.touches[0].phase);
+        }else{
+            if (Input.GetMouseButtonDown(0)){
+                HandleTouch(TouchPhase.Began);
+            }
+
+            if (Input.GetMouseButtonUp(0)){
+                HandleTouch(TouchPhase.Ended);
+            }
+
+            if (Input.GetMouseButton(0)){
+                HandleTouch(TouchPhase.Moved);
+            }
+        }
+
+        /*if (Input.touchCount > 0){
             switch (Input.touches[0].phase){
                 case TouchPhase.Moved:
                     yaw = speedH * Input.touches[0].deltaPosition.x;
@@ -26,10 +48,34 @@ public class Camara : MonoBehaviour {
 
             }
         }else{
-            yaw = speedH * Input.GetAxis("Mouse X");
+            /*yaw = speedH * Input.GetAxis("Mouse X");
             pitch = -(speedV * Input.GetAxis("Mouse Y"));
 
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x+pitch, transform.eulerAngles.y+yaw, transform.eulerAngles.z);
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x+pitch, transform.eulerAngles.y+yaw, transform.eulerAngles.z);*/
+
+            Transform c = transform;
+            c.Rotate(0, Input.GetAxis("Mouse X")* speedH, 0);
+            c.Rotate(-Input.GetAxis("Mouse Y")* speedV, 0, 0);
+            //c.Rotate(0, 0, -Input.GetAxis("QandE")*90 * Time.deltaTime);
+            /*if (Input.GetMouseButtonDown(0))
+                Cursor.lockState = CursorLockMode.Locked;*/
+        }
+    //}
+    void HandleTouch(TouchPhase phase){
+        switch (phase){
+            case TouchPhase.Began:
+                //posPulsado = 
+            break;
+
+            case TouchPhase.Ended:
+            case TouchPhase.Canceled:
+
+            break;
+
+            case TouchPhase.Moved:
+            case TouchPhase.Stationary:
+
+            break;
         }
     }
 }

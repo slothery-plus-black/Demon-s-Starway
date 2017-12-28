ba765ed96e029af180590e91cb64b74a77e8f42e
Jaime Calvo <jaime230593@hotmail.es>
2017-12-28 19:59:35 +0100

    vidas en jugador y reinicio de partida

--- Demon's Starway/Assets/Scripts/ChoquesJugador.cs
+++ Demon's Starway/Assets/Scripts/ChoquesJugador.cs
@@ -6,24 +6,51 @@ public class ChoquesJugador : MonoBehaviour {
 
 	PuntasEstrella puntas = new PuntasEstrella();
 
+	int vidas = 3;
+	Vector3 posInicial;
+	Vector3 posInicialCamara;
+	Quaternion posInicialRotation;
+
 	public ReproductorSonidos sonidos;
 	public AudioClip sonidoPunta;
 	public GameObject salida;
 
+	GameObject pivot;
+
 	// Use this for initialization
 	void Start () {
+		posInicial = transform.position;
 		
+		pivot = GameObject.Find("pivot camara");
+
+		posInicialCamara = pivot.transform.position;
+		posInicialRotation = pivot.transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//print(posInicialCamara.position);
+	}
+
+	void ComprobarEnemigo(GameObject other){
+		if (other.tag.ToLower().Equals("enemigo")){
+			vidas --;
+
+			if (vidas <= 0){
+				CargadorEscenas.CargaEscenaAsync("Menu");
+			}else{
+				pivot.transform.position = posInicialCamara;
+				pivot.transform.rotation = posInicialRotation;
+
+				transform.position = posInicial;
+			}
+		}
 	}
 
 	void OnTriggerEnter(Collider other){
 		
 		if (puntas.GetPuntas() >= 5 && other.gameObject.tag.ToLower().Equals("salida")){
-			CargadorEscenas.CargaEscenaAsync("mundo1");
+			CargadorEscenas.CargaEscenaAsync("Menu");
 		}
 
 		if (other.gameObject.tag.ToLower().Equals("punta")){
@@ -35,16 +62,10 @@ public class ChoquesJugador : MonoBehaviour {
 			}
 		}
 
-		if (other.gameObject.tag.ToLower().Equals("enemigo")){
-			//Se le quita una vida o muere
-			CargadorEscenas.CargaEscenaAsync("mundo1");
-		}
+		ComprobarEnemigo(other.gameObject);
 	}
 
 	void OnCollisionEnter(Collision other){
-		if (other.gameObject.tag.ToLower().Equals("enemigo")){
-			//Se le quita una vida o muere
-			CargadorEscenas.CargaEscenaAsync("mundo1");
-		}
+		ComprobarEnemigo(other.gameObject);
 	}
 }

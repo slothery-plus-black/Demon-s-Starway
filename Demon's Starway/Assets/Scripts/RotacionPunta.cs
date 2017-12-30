b72daab2858e459ba3cc882c149dfb5c1be26b15
Jaime Calvo <jaime230593@hotmail.es>
2017-12-30 15:14:11 +0100

    puntas bien lvl1

---
+++ Demon's Starway/Assets/Scripts/RotacionPunta.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionPunta : MonoBehaviour {
+
+	public float velocidad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0f,0f,velocidad * Time.deltaTime);
+	}
+}

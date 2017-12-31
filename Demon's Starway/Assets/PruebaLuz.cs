201c192ab556187ff77a3ce0d67eca75a40702f7
Yandrack <4a.ismael.cabrera@gmail.com>
2017-12-31 04:32:28 +0100

    Prueba Luz + Fixed Errors

---
+++ Demon's Starway/Assets/PruebaLuz.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaLuz : MonoBehaviour {
+	public float velTime = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate(velTime, velTime,0);
+	}
+}

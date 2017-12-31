3d2269bb5ed0d577251770cc76df2048f7100bb9
Jesus Brey <jesusbrey@gmail.com>
2017-12-31 20:33:13 +0100

    Preview del nivel

---
+++ Demon's Starway/Assets/Scripts/CambioCamara.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioCamara : MonoBehaviour {
+
+public Camera camera1;
+public Camera MainCamera;
+public float CountdownFrom;
+
+ void Start () {
+ 
+ camera1.enabled = true;
+ MainCamera.enabled = false;
+
+ }
+
+ void Update() {
+        float time = CountdownFrom - Time.timeSinceLevelLoad;
+ 
+        if(time<=0f) {
+            Switch();
+        }
+    }
+ 
+ void Switch(){
+ 
+     camera1.enabled = false;
+     MainCamera.enabled = true;
+     
+ }
+}

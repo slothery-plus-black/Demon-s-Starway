206f6ab2c6272524d37a28ad986f5b1bf59de451
Jaime Calvo <jaime230593@hotmail.es>
2018-01-02 17:34:10 +0100

    camara tour bien

---
+++ Demon's Starway/Assets/CargaIdiomaTexto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargaIdiomaTexto : MonoBehaviour {
+
+	public string textoEsp;
+	public string textoEng;
+
+	string spriteOriginal;
+
+	void Awake () {
+		spriteOriginal = GetComponent<SpriteRenderer>().sprite.name;
+		Cargar();
+	}
+
+	public void Cargar(){
+		string temp = "";
+
+		SpriteRenderer spr = GetComponent<SpriteRenderer>();
+
+		switch (PlayerPrefs.GetString("idioma","esp")){
+			case "esp":
+				temp = spriteOriginal;
+			break;
+
+			case "eng":
+				temp = spriteOriginal+"_eng";
+			break;
+		}
+
+		//Debug.Log(temp);
+
+		spr.sprite = Resources.Load<Sprite>(temp);
+	}
+}

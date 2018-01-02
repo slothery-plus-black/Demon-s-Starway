1cb869176fc9920e57459bc77eef3fb94c467110
Jaime Calvo <jaime230593@hotmail.es>
2018-01-02 15:19:12 +0100

    loby musica

---
+++ Demon's Starway/Assets/VolverDeLobby.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class VolverDeLobby : MonoBehaviour {
+
+	public NetworkLobbyManager manager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+		CargadorEscenas.CargaEscenaAsync("Menu");
+		manager.StopMatchMaker();
+		manager.StopClient();
+		manager.StopHost();
+		Destroy(manager.gameObject.GetComponent<NetworkManager>());
+		
+	}
+}
